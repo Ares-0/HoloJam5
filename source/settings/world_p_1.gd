4298605c58b2e8cc55b43d7ca1ce51d1ba83305c
Mason Hoppe <mason.hoppe@gmail.com>
2024-10-29 21:21:21 -0400

    Rename first batch of levels

--- source/settings/world_p_1.gd
+++ source/settings/world_p_1.gd
@@ -3,12 +3,12 @@ extends World
 func fill_room_list() -> void:
 	# This is just down here because I want it at the bottom of the file
 	room_list = [
-		"res://source/settings/rooms/p1/r1.tscn", # jump and move right
-		"res://source/settings/rooms/p1/r2.tscn", # jump over pits
-		"res://source/settings/rooms/p1/r3.tscn", # dash over large pits
-		"res://source/settings/rooms/p1/r4.tscn", # dash up (and down)
-		"res://source/settings/rooms/p1/r5.tscn", # need to cleanse stars to exit
-		"res://source/settings/rooms/p1/r6.tscn", # stars can have multiple charges
+		"res://source/settings/rooms/p1/a1.tscn", # jump and move right
+		"res://source/settings/rooms/p1/a2.tscn", # jump over pits
+		"res://source/settings/rooms/p1/a3.tscn", # dash over large pits
+		"res://source/settings/rooms/p1/a4.tscn", # dash up (and down)
+		"res://source/settings/rooms/p1/a5.tscn", # need to cleanse stars to exit
+		"res://source/settings/rooms/p1/a6.tscn", # stars can have multiple charges
 
 		"res://source/settings/rooms/p2/d1.tscn",
 		"res://source/settings/rooms/p2/d2.tscn",
@@ -16,5 +16,8 @@ func fill_room_list() -> void:
 		"res://source/settings/rooms/p2/d4.tscn",
 		"res://source/settings/rooms/p2/d5.tscn",
 		"res://source/settings/rooms/p2/d6.tscn",
+		
+		"res://source/settings/rooms/p2/d7.tscn",
+		"res://source/settings/rooms/p2/d8.tscn",
 	]
 	room_count = room_list.size()
